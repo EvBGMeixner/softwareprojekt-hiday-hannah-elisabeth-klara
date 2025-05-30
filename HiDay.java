3021986db12b7b1649dd189e7ece8811ada35757
Elisabeth <elisabeth.ritzhaupt-kleissl@student.evbg.de>
2025-05-30 10:36:33 +0200

    hallihallohallöchen

--- HiDay.java
+++ HiDay.java
@@ -1,11 +1,13 @@
 
 public class HiDay extends SPIEL{
 
-    
+    FIGUR figur;
     
 
-    public HiDay(){
+    public HiDay(String _zustandName , String _gifPfad){
      zeigeKoordinatensystem(true);
+     figur = new FIGUR(_zustandName , _gifPfad);
+     
     
     }
 
