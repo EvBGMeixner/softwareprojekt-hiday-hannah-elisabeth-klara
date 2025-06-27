bd1e8fa2e262460a580891178e9485bece7a169f
Klara <klara.pichl@student.evbg.de>
2025-06-27 09:30:56 +0200

    farmer kann laufen springen hoffentlich

--- HiDay.java
+++ HiDay.java
@@ -1,20 +1,37 @@
 
 public class HiDay extends SPIEL{
 
-    HiDayFigur figur;
+    HiDayFigur farmer;
 
     public HiDay(){
         super();
 
         zeigeKoordinatensystem(true);
 
-        figur=new HiDayFigur();
-        figur.setzeMittelpunkt(0, -8);
+        farmer=new HiDayFigur();
+        farmer.setzeMittelpunkt(0, -8);
 
 
     }
-    void setzeKamerafokus (HiDayFigur figur){
-        super.setzeKamerafokus( figur );
+    
+    void setzeKamerafokus (HiDayFigur farmer){
+        super.setzeKamerafokus( farmer );
+    }
+    
+    public void tasteReagieren(int taste){
+        if(taste == TASTE.RAUF){
+            farmer.springe(12);
+        }
+        
+    }
+
+    public void bildAktualisierungReagieren(double s){
+        if(istTasteGedrueckt(TASTE.RECHTS)){
+            farmer.verschiebenUm(0.1, 0);
+        }
+        if(istTasteGedrueckt(TASTE.LINKS)){
+            farmer.verschiebenUm(-0.1, 0);
+        }
     }
 }
 
