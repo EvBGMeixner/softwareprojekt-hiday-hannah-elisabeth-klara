688575e4f794b73d2cb7fb399b3fabc74bb4b239
Hannah <hannah.soehner@student.evbg.de>
2025-06-27 10:11:04 +0200

    ,,

--- HiDay.java
+++ HiDay.java
@@ -6,33 +6,26 @@ public class HiDay extends SPIEL{
     public HiDay(){
         super();
 
-        
         zeigeKoordinatensystem(true);
-
         farmer=new HiDayFigur();
         farmer.setzeMittelpunkt(0, -8);
-        
 
-
-    
 
 
 
     }
-    
+
     void setzeKamerafokus (HiDayFigur farmer){
         super.setzeKamerafokus( farmer );
     }
-    
-    
 
-    
+
     
     public void tasteReagieren(int taste){
         if(taste == TASTE.RAUF){
             farmer.springe(12);
         }
-        
+
     }
 
     public void bildAktualisierungReagieren(double s){
@@ -44,10 +37,8 @@ public class HiDay extends SPIEL{
         }
 
     }
-    
 
     // Figur hinzufügen
-    
    
 }
 
