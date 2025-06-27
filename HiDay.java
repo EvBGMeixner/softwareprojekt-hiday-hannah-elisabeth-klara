b4607aa75cb2e795d24555fb93a7a8f8dbed3ce7
Elisabeth <elisabeth.ritzhaupt-kleissl@student.evbg.de>
2025-06-27 09:32:50 +0200

    p

--- HiDay.java
+++ HiDay.java
@@ -1,22 +1,40 @@
 
 public class HiDay extends SPIEL{
 
-    HiDayFigur figur;
+    HiDayFigur farmer;
 
     public HiDay(){
         super();
 
-        zeigeKoordinatensystem(true);
-
+        zeigeKoordinatensystem(true); 
         figur = new HiDayFigur();
         figur.setzeMittelpunkt(0, -8);
         
 
 
+
+    
+
+    public void setzeKamerafokus (HiDayFigur figur){
+        super.setzeKamerafokus( figur );
+
+    }
+    
+    public void tasteReagieren(int taste){
+        if(taste == TASTE.RAUF){
+            farmer.springe(12);
+        }
+        
     }
 
-    void setzeKamerafokus (HiDayFigur figur){
-        super.setzeKamerafokus( figur );
+    public void bildAktualisierungReagieren(double s){
+        if(istTasteGedrueckt(TASTE.RECHTS)){
+            farmer.verschiebenUm(0.1, 0);
+        }
+        if(istTasteGedrueckt(TASTE.LINKS)){
+            farmer.verschiebenUm(-0.1, 0);
+        }
+
     }
     
 
