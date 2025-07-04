f49463c9eb61871889241371a1ce10e7f0cb457d
Hannah <hannah.soehner@student.evbg.de>
2025-07-04 10:02:31 +0200

    jugvu

--- HiDay.java
+++ HiDay.java
@@ -4,14 +4,18 @@ public class HiDay extends SPIEL{
     HiDayFigur farmer;
     RECHTECK boden;
 
+    String punkteanzeige;
+    int x;
+    String lebensanzeige;
+    int y;
     public HiDay(){
         super();
 
         zeigeKoordinatensystem(true);
         farmer=new HiDayFigur();
 
+
         setzeKamerafokus (farmer);
-       
 
         farmer.setzeMittelpunkt(0, -7.5);
         boden = new RECHTECK(1000,1);
@@ -26,16 +30,36 @@ public class HiDay extends SPIEL{
         hintergrundbild.machePassiv();
 
        
-       
         
-
-
         
-    
+        x = 0;
+        y = 3;
 
+        setzeKamerafokus (farmer);
+        punkteanzeige = new String("Punkte:int x");
+        lebensanzeige = new String("Leben:int y");
+
+        farmer.setzeMittelpunkt(0, -7.5);
+        boden = new RECHTECK(1000,1);
+        boden.setzeMittelpunkt(480,-10);
+        boden.setzeFarbe("grün");
+
+        setzeHintergrundgrafik("bauernhofhintergrund.png");
+        
+        farmer.macheAktiv();
+        boden.machePassiv();
+
+        hintergrundbild.machePassiv();
+        lebensanzeige = y;
+        punkteanzeige = x;
+        
+        
+        
+        
+        
     }
 
-
+    
     public void tasteReagieren(int taste){
         if(taste == TASTE.RAUF){
             farmer.springe(12);
@@ -52,5 +76,13 @@ public class HiDay extends SPIEL{
         }
 
     }
-}
-    
+
+    //public void anzeigeErstellen(){
+        //lebensanzeige.setzeMittelpunkt(5,7);
+       // punkteanzeige.setzeMittelpunkt(5,6);
+        //lebensanzeige.setzeFarbe("rot");
+       // punkteanzeige.setzeFarbe("blau");
+
+    }
+    // Figur hinzufügen
+
